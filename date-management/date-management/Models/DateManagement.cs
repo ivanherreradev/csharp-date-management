4b22721b24106dcbddbf1d40ef1cdd5f6f5acbcc
ivanherreradev <herreraivan@outlook.com.ar>
2024-01-09 09:15:35 -0300

    completed exercise

---
+++ date-management/date-management/Models/DateManagement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace date_management.Models
+{
+    public class DateManagement
+    {
+        public int GetCalendarDays(DateTime startDate, DateTime endDate) 
+        {
+            TimeSpan differenceBetweenStartAndEndDates = endDate - startDate;
+            return differenceBetweenStartAndEndDates.Days;
+        }
+
+        public int GetWorkingDays(DateTime startDate, DateTime endDate, List<DateTime> holidays) 
+        {
+            int workingDays = 0;
+            DateTime currentDate = startDate;
+
+            while (currentDate <= endDate)
+            {
+                if (IsWorkingDay(currentDate, holidays))
+                {
+                    workingDays++;
+                }
+
+                currentDate = currentDate.AddDays(1);
+            }
+
+            return workingDays;
+        }
+
+        public DateTime AddWorkingDays(DateTime startDate,int workingDays, List<DateTime> holidays) 
+        {
+            DateTime currentDate = startDate;
+
+            while (workingDays > 0)
+            {
+                currentDate = currentDate.AddDays(1);
+
+                if (IsWorkingDay(currentDate, holidays))
+                {
+                    workingDays--;
+                }
+            }
+
+            return currentDate;
+        }
+
+        // Check if a day is a working day, taking into account weekends and holidays.
+        private static bool IsWorkingDay(DateTime date, List<DateTime> holidays)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return false;
+            }
+
+            if (holidays.Contains(date))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ date-management/date-management/Program.cs
@@ -0,0 +1,21 @@
+﻿using date_management.Models;
+
+DateTime startDate = new DateTime(2024, 1, 1);
+DateTime endDate = new DateTime(2024, 1, 15);
+
+DateManagement dateManagement = new DateManagement();
+
+Console.WriteLine($"Days between '{startDate}' and '{endDate}' is {dateManagement.GetCalendarDays(startDate, endDate)} days.");
+
+List<DateTime> holidays = new List<DateTime>
+{
+    new DateTime(2024, 1, 4),
+    new DateTime(2024, 1, 10) 
+};
+
+Console.WriteLine($"Working days between '{startDate}' and '{endDate}' is {dateManagement.GetWorkingDays(startDate, endDate, holidays)} days.");
+
+int workingDaysToAdd = 5;
+DateTime newDate = dateManagement.AddWorkingDays(startDate, workingDaysToAdd, holidays);
+
+Console.WriteLine($"New date adding '{workingDaysToAdd}' working days to '{startDate}' is: {newDate}");
